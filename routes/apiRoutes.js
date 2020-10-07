61d076a60de725c3953f91f1466e389cdcadf8f7
Chambers Mansour <chambersmansour97@gmail.com>
2020-10-07 02:41:36 -0500

    finished get request and framework for post reques

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+//connects data to array we are going to store it in
+var noteContents = require("../db/noteData");
+
+//Creates the route to send our get and post and delete requests
+module.exports = function(app) {
+
+    //Display all notes
+    app.get("/api/notes", function(req, res) {
+        res.json(noteContents)
+    })
+
+    //Post a note
+    app.post("/api/notes", function(req,res) {
+        noteContents.push(req.body);
+    })
+}
+
+
+
+
+//part of delete request
+for (let i = 0; i < noteContents.length; i++)
